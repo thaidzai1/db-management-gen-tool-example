b3ca777ad1b16c7974bac5419f95f9b6b426a228
Quang Nguyen <quang.astronaut@gmail.com>
2019-12-18 13:43:45 +0700

    Update history for vendor_fee_tier,vendor

--- git_msg.sh
+++ git_msg.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-curl -H "PRIVATE-TOKEN: $API_TOKEN" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHA" | jq '.message'
 msg=$(curl -H "PRIVATE-TOKEN: $API_TOKEN" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHA" | jq '.message')
 echo $msg | grep -o "[^\"]*"
