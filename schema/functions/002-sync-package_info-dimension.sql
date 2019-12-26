9fa83f79e9e13c085f0f6e7a2a2d5520afa25bfc
Quang Nguyen <quang.astronaut@gmail.com>
2019-12-26 12:05:12 +0700

    Add actual length width, height for package_info

---
+++ schema/functions/002-sync-package_info-dimension.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION public.sync_order_item_package_info() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    rec record;
+    changes JSONB;
+BEGIN
+    IF NEW.package_info_id > 0 AND NEW.state = '3A' AND OLD.state = '1SZ' AND NEW.wh_inventory_item_id > 0 THEN
+        SELECT * INTO rec FROM wh_inventory_item WHERE id = NEW.wh_inventory_item_id LIMIT 1;
+        UPDATE package_info set actual_weight = NEW.real_weight, actual_width = rec.width, actual_length = rec.length, actual_height = rec.height WHERE id = NEW.package_info_id;
+    END IF;
+    RETURN NULL;
+END
+$$;
