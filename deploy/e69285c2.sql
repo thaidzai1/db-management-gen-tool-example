2c25c2bf551bc65a0cd15cfb36e1c0f2a62e93ca
Mr. Robot <gitbot@gido.vn>
2020-01-06 04:21:26 +0000

    [ROBOT] Auto generate deploy from [64876](https://g.ghn.vn/gic/databases/gido-v1.0/-/jobs/64876)

---
+++ deploy/e69285c2.sql
@@ -0,0 +1,27 @@
+
+BEGIN;
+
+/*-- TRIGGER BEGIN --*/
+CREATE OR REPLACE FUNCTION public.sync_order_price_wallet_transaction() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    rec record;
+    changes JSONB;
+BEGIN
+    IF NEW.state <> '' THEN
+        SELECT * INTO rec FROM order_price WHERE wallet_transaction_id = NEW.id LIMIT 1;
+        IF rec IS NOT NULL THEN
+            UPDATE order_price set wallet_transaction_state = NEW.state WHERE id = rec.id;
+        END IF;
+    END IF;
+    RETURN NULL;
+END
+$$; 
+
+CREATE TRIGGER sync_order_price_wallet_transaction AFTER UPDATE ON wallet_transaction
+    FOR EACH ROW EXECUTE PROCEDURE sync_order_price_wallet_transaction();
+    
+/*-- TRIGGER END --*/
+
+COMMIT;
