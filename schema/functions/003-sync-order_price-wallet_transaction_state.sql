eff3a0a9d978567ba87d892e65aa23a6477cc794
ThaiDzai <thaidzai@dzaiBook.local>
2020-01-06 17:23:32 +0700

    fix bug of sqitch deploy always failed

--- schema/functions/003-sync-order_price-wallet_transaction_state.sql
+++ schema/functions/003-sync-order_price-wallet_transaction_state.sql
@@ -7,13 +7,10 @@ DECLARE
 BEGIN
     IF NEW.state <> '' THEN
         SELECT * INTO rec FROM order_price WHERE wallet_transaction_id = NEW.id LIMIT 1;
-        IF rec <> null THEN
+        IF (rec.id IS NOT NULL) THEN
             UPDATE order_price set wallet_transaction_state = NEW.state WHERE id = rec.id;
         END IF;
     END IF;
     RETURN NULL;
 END
 $$; 
-
-CREATE TRIGGER sync_order_price_wallet_transaction AFTER UPDATE ON wallet_transaction
-    FOR EACH ROW EXECUTE PROCEDURE sync_order_price_wallet_transaction();
